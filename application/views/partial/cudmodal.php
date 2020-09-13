e03ac8af0806da11a09b7187f607b54d4b1c1bb8
quifar <risqufaris@gmail.com>
2020-09-13 21:34:30 +0700

    Create cudmodal.php

---
+++ application/views/partial/cudmodal.php
@@ -0,0 +1,51 @@
+<!-- Modal Insert-->
+<div class="modal fade" id="insertModal" data-backdrop="static" data-keyboard="false" tabindex="-1"
+		aria-labelledby="staticBackdropLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="staticBackdropLabel">Insert Customer</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body" id="insert-data">
+				</div>
+			</div>
+		</div>
+	</div>
+
+<!-- Modal Edit-->
+<div class="modal fade" id="editModal" data-backdrop="static" data-keyboard="false" tabindex="-1"
+		aria-labelledby="staticBackdropLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="staticBackdropLabel">Edit Customer</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body" id="update-data">
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Modal Delete-->
+	<div class="modal fade" id="deleteModal" data-backdrop="static" data-keyboard="false" tabindex="-1"
+		aria-labelledby="staticBackdropLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="staticBackdropLabel">Hapus Customer</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body" id="delete-data">
+
+				</div>
+			</div>
+		</div>
+	</div>
