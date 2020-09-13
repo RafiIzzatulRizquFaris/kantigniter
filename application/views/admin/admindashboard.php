9e70889556ebb2c22542b8c897bc88fb513fa0fe
quifar <risqufaris@gmail.com>
2020-09-13 19:42:59 +0700

    add sidebaradmin partial

--- application/views/admin/admindashboard.php
+++ application/views/admin/admindashboard.php
@@ -83,62 +83,9 @@
 	<div id="wrapper">
 
 		<!-- Sidebar -->
-		<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-			<!-- Sidebar - Brand -->
-			<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-				<div class="sidebar-brand-icon rotate-n-15">
-					<i class="fa fa-cutlery" aria-hidden="true"></i>
-				</div>
-				<div class="sidebar-brand-text mx-3">Kantigniter</div>
-			</a>
-
-			<!-- Divider -->
-			<hr class="sidebar-divider">
-
-			<div class="sidebar-heading">
-				Dashboard
-			</div>
-
-			<!-- Nav Item - Dashboard -->
-			<li class="nav-item active">
-				<a class="nav-link" href="<?php echo site_url('Admin/index');?>">
-					<i class="fas fa-fw fa-database"></i>
-					<span>Data Pengaduan</span></a>
-			</li>
-
-			<li class="nav-item">
-				<a class="nav-link" href="<?php echo site_url('OfficerDataController/index');?>">
-					<i class="fas fa-fw fa-database"></i>
-					<span>Data Petugas</span></a>
-			</li>
-
-			<!-- Nav Item - Charts -->
-			<li class="nav-item">
-				<a class="nav-link" href="<?php echo site_url('OfficerInputController/index');?>">
-					<i class="fas fa-fw fa-envelope-open"></i>
-					<span>Input Petugas</span></a>
-			</li>
-
-			<!-- Divider -->
-			<hr class="sidebar-divider">
-
-			<!-- Heading -->
-			<div class="sidebar-heading">
-				Account
-			</div>
-
-			<!-- Nav Item - Tables -->
-			<li class="nav-item">
-				<a class="nav-link" data-toggle="modal" data-target="#logoutModal">
-					<i class="fa fa-minus-circle"></i>
-					<span>Sign Out</span></a>
-			</li>
-
-			<!-- Divider -->
-			<hr class="sidebar-divider d-none d-md-block">
-
-		</ul>
+		<?php
+            $this->load->view('partial/sidebaradmin');
+        ?>
 		<!-- End of Sidebar -->
 
 		<!-- Content Wrapper -->
