a9e6e724ffa6d05dd8063245a9bca4ac71483f80
quifar <risqufaris@gmail.com>
2020-09-13 21:34:39 +0700

    Create DataModel.php

---
+++ application/models/DataModel.php
@@ -0,0 +1,18 @@
+<?php
+class DataModel extends CI_Model{
+    public function readCustomer()
+    {
+        $data = $this->db->get('customer');
+        return $data->result();
+    }
+
+    public function deleteCustomer($data)
+    {
+        $this->db->delete('customer', $data);
+    }
+
+    public function updateCustomer($data, $where)
+    {
+        $this->db->update('customer', $data, $where);
+    }
+}
