3db8d08faf1b2cd2b01efac0ccd272a9d13f9b35
quifar <risqufaris@gmail.com>
2020-09-13 16:01:29 +0700

    Update Admin.php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -11,8 +11,9 @@ class Admin extends CI_Controller{
     public function index()
     {
         if ($this->session->userdata('status') == 'login' && $this->session->userdata('role') == '1' && $this->session->userdata('state') == 'aktif')  {
-            $data['pengaduan'] = $this->ModelAction->get_pengaduan();
-            $this->load->view('admin/admindashboard', $data);
+            // $data['pengaduan'] = $this->ModelAction->get_pengaduan();
+            // $this->load->view('admin/admindashboard', $data);
+            $this->load->view('admin/admindashboard');
         } else {
             header("Location:".base_url().'Login/index');
         }
