e83a0e67349a8e8431ffab1c8005aae96f3cb50d
quifar <risqufaris@gmail.com>
2020-09-14 22:09:24 +0700

    change top icon

--- application/views/partial/sidebaradmin.php
+++ application/views/partial/sidebaradmin.php
@@ -1,9 +1,9 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
 			<!-- Sidebar - Brand -->
-			<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+			<a class="sidebar-brand d-flex align-items-center justify-content-center">
 				<div class="sidebar-brand-icon rotate-n-15">
-					<i class="fa fa-cutlery" aria-hidden="true"></i>
+				<i class="fas fa-cookie-bite"></i>
 				</div>
 				<div class="sidebar-brand-text mx-3">Kantigniter</div>
 			</a>
