9944f61982d969dc0f4677272ea824531bb0e691
quifar <risqufaris@gmail.com>
2020-09-14 22:02:06 +0700

    Create sidebarwaiter.php

---
+++ application/views/partial/sidebarwaiter.php
@@ -0,0 +1,56 @@
+<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+			<!-- Sidebar - Brand -->
+			<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+				<div class="sidebar-brand-icon rotate-n-15">
+					<i class="fa fa-cutlery" aria-hidden="true"></i>
+				</div>
+				<div class="sidebar-brand-text mx-3">Kantigniter</div>
+			</a>
+
+			<!-- Divider -->
+			<hr class="sidebar-divider">
+
+			<div class="sidebar-heading">
+				Dashboard
+			</div>
+
+			<!-- Nav Item - Dashboard -->
+			<li class="nav-item <?php echo $this->uri->segment(2) == 'index' ? 'active': '' ?>">
+				<a class="nav-link" href="<?php echo site_url('Waiter/index');?>">
+                <i class="fas fa-fw fa-clipboard"></i>
+					<span>Order Input</span></a>
+			</li>
+
+			<li class="nav-item <?php echo $this->uri->segment(2) == 'dataorder' ? 'active': '' ?>">
+				<a class="nav-link" href="<?php echo site_url('Waiter/dataorder');?>">
+					<i class="fas fa-fw fa-database"></i>
+					<span>Data Order</span></a>
+			</li>
+
+			<!-- Nav Item - Charts -->
+			<li class="nav-item <?php echo $this->uri->segment(2) == 'dataproduct' ? 'active': '' ?>">
+				<a class="nav-link" href="<?php echo site_url('Waiter/dataproduct');?>">
+					<i class="fas fa-fw fa-database"></i>
+					<span>Data Product</span></a>
+			</li>
+
+			<!-- Divider -->
+			<hr class="sidebar-divider">
+
+			<!-- Heading -->
+			<div class="sidebar-heading">
+				Account
+			</div>
+
+			<!-- Nav Item - Tables -->
+			<li class="nav-item">
+				<a class="nav-link" data-toggle="modal" data-target="#logoutModal">
+					<i class="fa fa-minus-circle"></i>
+					<span>Sign Out</span></a>
+			</li>
+
+			<!-- Divider -->
+			<hr class="sidebar-divider d-none d-md-block">
+
+		</ul>
