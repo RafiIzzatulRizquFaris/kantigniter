931254d798254cc7457b466fd5865b0825d43df3
quifar <risqufaris@gmail.com>
2020-09-14 22:04:12 +0700

    Update cudmodal.php
    change title CUD modal

--- application/views/partial/cudmodal.php
+++ application/views/partial/cudmodal.php
@@ -4,7 +4,7 @@
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title" id="staticBackdropLabel">Insert Customer</h5>
+					<h5 class="modal-title" id="staticBackdropLabel">Insert</h5>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
@@ -21,7 +21,7 @@
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title" id="staticBackdropLabel">Edit Customer</h5>
+					<h5 class="modal-title" id="staticBackdropLabel">Update</h5>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
@@ -38,7 +38,7 @@
 		<div class="modal-dialog">
 			<div class="modal-content">
 				<div class="modal-header">
-					<h5 class="modal-title" id="staticBackdropLabel">Hapus Customer</h5>
+					<h5 class="modal-title" id="staticBackdropLabel">Delete</h5>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
 					</button>
