1a845d6992cb1d72c9b66d1e306335c76e83fd74
quifar <risqufaris@gmail.com>
2020-09-14 22:51:08 +0700

    Update waiterdashboard.php
    change layout

--- application/views/waiter/waiterdashboard.php
+++ application/views/waiter/waiterdashboard.php
@@ -128,7 +128,6 @@
 					</div>`
 			$("#insert-data").append(layoutupdate)
 		}
-
 	</script>
 </head>
 
@@ -149,64 +148,24 @@
 			<!-- Main Content -->
 			<div id="content">
 
-			<?php
+				<?php
              $this->load->view('partial/topbar');
             ?>
 
 				<!-- Begin Page Content -->
-				<div class="container-fluid">
-					<!-- Page Heading -->
-					<h1 class="h3 mb-2 text-gray-800">Input Pengaduan</h1>
-					<p class="mb-4">
-						Masukkan semua field yang ada untuk mebuat pengaduan, customer
-						service kami akan merespon secepatnya
-					</p>
+				<div class="container">
 
-					<div class="card shadow mb-4">
-						<div class="card-header py-3 text-center">
-							<h6 class="m-0 font-weight-bold text-primary">
-								Form Pengaduan
-							</h6>
-						</div>
-						<div class="card-body">
-							<div class="card card-7">
-								<div class="card-body">
-									<form method="POST" action="<?php echo site_url('AduanController/insertLaporan'); ?>" enctype="multipart/form-data">
-										<div class="form-row">
-											<div class="name">Judul Pengaduan</div>
-											<div class="value">
-												<div class="input-group">
-													<input class="input--style-5" type="text" name="judul_report" />
-												</div>
-											</div>
-										</div>
-										<div class="form-row">
-											<div class="name">Isi Pengaduan</div>
-											<div class="value">
-												<div class="input-group text-justify">
-													<!-- <input type="text" class="input--style-5 input-group-text" name="isi_pengaduan"> -->
-													<textarea class="form-control input--style-5 input-group-text" name="isi_report"></textarea>
-												</div>
-											</div>
-										</div>
-										<div class="form-row">
-											<div class="name">Bukti Pengaduan</div>
-											<div class="value">
-												<div class="input-group">
-													<input type="file" class="form-control-file input--style-5" id="bukti_pengaduan" name="foto_report">
-												</div>
-											</div>
-										</div>
-										<div class="text-center">
-											<button class="btn btn--radius-2 btn-success btn-block btn-lg" type="submit">
-												Submit
-											</button>
-										</div>
-									</form>
-								</div>
+					<div class="container">
+						<div class="row">
+							<div class="col-xl-8 col-lg-7">
+								<?php $this->load->view('partial/listproduct'); ?>
+							</div>
+							<div class="col-xl-4 col-lg-5">
+								<?php $this->load->view('partial/chart'); ?>
 							</div>
 						</div>
 					</div>
+
 				</div>
 				<!-- /.container-fluid -->
 
