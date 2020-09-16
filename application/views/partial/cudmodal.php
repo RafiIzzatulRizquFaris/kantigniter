3e7355bd641ddaf0cff7ae2efc09236ecf2e0125
quifar <risqufaris@gmail.com>
2020-09-16 23:15:40 +0700

    Update cudmodal.php
    add modal info

--- application/views/partial/cudmodal.php
+++ application/views/partial/cudmodal.php
@@ -49,3 +49,24 @@
 			</div>
 		</div>
 	</div>
+
+	<!-- Modal Info-->
+	<div class="modal fade" id="infoModal" data-backdrop="static" data-keyboard="false" tabindex="-1"
+		aria-labelledby="staticBackdropLabel" aria-hidden="true">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="staticBackdropLabel">Tanggapan</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div id="detail_result"></div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-primary" data-dismiss="modal">Ok</button>
+				</div>
+			</div>
+		</div>
+	</div>
